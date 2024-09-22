5a8e9851f6d009a416fb38bd9a1a50109889796a
lilou <lilou@10.3.62.38>
2024-09-22 21:43:43 +0200

    MaJ Calculator

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -4,8 +4,11 @@
  */
 package calculator;
 
+import java.util.Scanner;
+
 /**
  * MINOIS Lilou TDC 20/09/2024
+ *
  * @author lilou
  */
 public class Calculator {
@@ -15,6 +18,49 @@ public class Calculator {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Please enter the operator:");
+        System.out.println("1) add");
+        System.out.println("2) substract");
+        System.out.println("3) multiply");
+        System.out.println("4) divide");
+        System.out.println("5) modulo");
+        Scanner sc;
+        sc = new Scanner(System.in);
+        int operateur;
+        operateur = sc.nextInt();
+        while (operateur < 1 || operateur > 5) {
+            System.out.println("Invalid operator. Please enter a number 1 and 5:");
+            operateur = sc.nextInt();
+        }
+
+        System.out.println("Please enter the first number:");
+        int operande1 = sc.nextInt();
+        System.out.println("Please enter the second number:");
+        int operande2 = sc.nextInt();
+
+        int result = 0;
+
+        switch (operateur) {
+            case 1:
+                result = operande1 + operande2;
+                break;
+            case 2:
+                result = operande1 - operande2;
+                break;
+            case 3:
+                result = operande1 * operande2;
+                break;
+            case 4:
+                result = operande1 / operande2;
+                break;
+            case 5:
+                result = operande1 % operande2;
+                break;
+            default:
+                System.err.println("erreur de saisie");
+        }
+        System.out.println("The result is: " + result);
+
     }
-    
+
 }
