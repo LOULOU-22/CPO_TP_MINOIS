8a938586ea807af9a1e9b00d4ac2cf4602f19845
lilou <lilou@10.3.62.38>
2024-09-22 20:35:29 +0200

    v1

---
+++ Calculator/src/calculator/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ * MINOIS Lilou TDC 20/09/2024
+ * @author lilou
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
