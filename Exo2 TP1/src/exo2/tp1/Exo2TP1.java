54aa11a8519c8e9a6a2f9f7c84f026f7e1dfb739
lilou <lilou@10.3.62.38>
2024-11-13 11:40:21 +0100

    Exo 1 fini

---
+++ Exo2 TP1/src/exo2/tp1/Exo2TP1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo2.tp1;
+import java.util.Scanner;
+/**
+ ** MINOIS Lilou TDC 20/09/2024
+ * @author lilou
+ */
+public class Exo2TP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double var1; // variable 1
+        double kelvin; 
+        int var2 ; // variable 2
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Saisir une valeur réelle : ");
+        var1 = sc.nextDouble();
+        kelvin = var1 + 273;
+        System.out.println("\n votre température en Kelvin est de : " + kelvin +"K");
+    
+    
+   
+         
+         
+   
